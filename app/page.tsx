300477b079f40a97d9cdf5285fb121e60e670fed
AmRo <VirtualEmailBox045@Gmail.Com>
2024-11-23 09:42:13 +0330

    Login page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,22 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
+import { Input } from "@nextui-org/input";
+import { Button } from "@nextui-org/button";
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-
-export default function Home() {
+export default function LoginPage() {
     return (
-        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-            <div className="inline-block max-w-xl text-center justify-center">
-                <span className={title()}>Make&nbsp;</span>
-                <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-                <br />
-                <span className={title()}>websites regardless of your design experience.</span>
-                <div className={subtitle({ class: "mt-4" })}>Beautiful, fast and modern React UI library.</div>
-            </div>
+        <form className="flex flex-col items-center justify-center gap-2 min-h-[64vh]">
+            <Input
+                className="w-[264px]"
+                color="primary"
+                label="Password"
+                placeholder="Enter your password"
+                required={true}
+                type="password"
+                variant="underlined"
+            />
 
-            <div className="flex gap-3">
-                <Link
-                    isExternal
-                    className={buttonStyles({
-                        color: "primary",
-                        radius: "full",
-                        variant: "shadow"
-                    })}
-                    href={siteConfig.links.docs}
-                >
-                    Documentation
-                </Link>
-                <Link
-                    isExternal
-                    className={buttonStyles({ variant: "bordered", radius: "full" })}
-                    href={siteConfig.links.github}
-                >
-                    <GithubIcon size={20} />
-                    GitHub
-                </Link>
-            </div>
-
-            <div className="mt-8">
-                <Snippet hideCopyButton hideSymbol variant="bordered">
-                    <span>
-                        Get started by editing <Code color="primary">app/page.tsx</Code>
-                    </span>
-                </Snippet>
-            </div>
-        </section>
+            <Button className="w-[264px]" size="sm" type="submit">
+                Login
+            </Button>
+        </form>
     );
 }
