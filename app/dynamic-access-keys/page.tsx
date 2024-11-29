9c23b82a5ee0f7c53f2e4e83cff6e375ba8a14d5
AmRo <VirtualEmailBox045@Gmail.Com>
2024-11-29 12:40:09 +0330

    Remove unused cmp

--- app/dynamic-access-keys/page.tsx
+++ app/dynamic-access-keys/page.tsx
@@ -1,9 +1,7 @@
-import { title } from "@/components/primitives";
-
 export default function DynamicAccessKeysPage() {
     return (
         <div>
-            <h1 className={title()}>DynamicAccessKeysPage</h1>
+            <h1>DynamicAccessKeysPage</h1>
         </div>
     );
 }
