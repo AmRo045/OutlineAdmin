3f7f9ff88dba8357b4e7cb5b5f2697b3c25b95c9
AmRo <VirtualEmailBox045@Gmail.Com>
2025-01-10 22:36:50 +0330

    Improve login page UI

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -4,6 +4,7 @@ import { useForm } from "react-hook-form";
 import { Button, Input } from "@nextui-org/react";
 
 import { checkPassword, login } from "@/src/core/actions";
+import { Logo } from "@/src/components/icons";
 
 interface FormProps {
     password: string;
@@ -27,6 +28,10 @@ export default function LoginForm() {
             className="flex flex-col items-center justify-center gap-2 min-h-[64vh]"
             onSubmit={form.handleSubmit(actualSubmit)}
         >
+            <div className="mb-8">
+                <Logo size={86} />
+            </div>
+
             <Input
                 className="w-[264px]"
                 color="primary"
