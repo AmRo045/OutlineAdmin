a15b2c3a2acacf589b93fa7e4a6401b0d187aa42
AmRo <VirtualEmailBox045@Gmail.Com>
2025-01-12 20:32:43 +0330

    Fix tailwind config issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,10 @@ import { nextui } from "@nextui-org/theme";
 /** @type {import("tailwindcss").Config} */
 module.exports = {
     content: [
-        "./src/(app|components|hooks)/**/*.{js,ts,jsx,tsx}",
-        "node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/hooks/**/*.{js,ts,jsx,tsx,mdx}",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
     ],
     theme: {
         extend: {
