67fc18e5810f40710db341bc30e7116bc0628bb9
AmRo <VirtualEmailBox045@Gmail.Com>
2025-11-07 15:57:09 +0330

    Add dataLimit field to migration

--- prisma/migrations/20251107062532_add_usage_interval_related_fields_to_dak/migration.sql
+++ prisma/migrations/20251107062532_add_usage_interval_related_fields_to_dak/migration.sql
@@ -14,11 +14,12 @@ CREATE TABLE "new_DynamicAccessKey" (
     "usageInterval" TEXT,
     "usageStartedAt" DATETIME,
     "dataUsage" BIGINT NOT NULL DEFAULT 0,
+    "dataLimit" INTEGER,
     "expiresAt" DATETIME,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
-INSERT INTO "new_DynamicAccessKey" ("createdAt", "expiresAt", "id", "isSelfManaged", "loadBalancerAlgorithm", "name", "path", "prefix", "serverPoolType", "serverPoolValue", 'activeServerId', "updatedAt") SELECT "createdAt", "expiresAt", "id", "isSelfManaged", "loadBalancerAlgorithm", "name", "path", "prefix", "serverPoolType", "serverPoolValue", "activeServerId", "updatedAt" FROM "DynamicAccessKey";
+INSERT INTO "new_DynamicAccessKey" ("createdAt", "expiresAt", "id", "isSelfManaged", "loadBalancerAlgorithm", "name", "path", "prefix", "serverPoolType", "serverPoolValue", "activeServerId", "updatedAt") SELECT "createdAt", "expiresAt", "id", "isSelfManaged", "loadBalancerAlgorithm", "name", "path", "prefix", "serverPoolType", "serverPoolValue", "activeServerId", "updatedAt" FROM "DynamicAccessKey";
 DROP TABLE "DynamicAccessKey";
 ALTER TABLE "new_DynamicAccessKey" RENAME TO "DynamicAccessKey";
 CREATE UNIQUE INDEX "DynamicAccessKey_path_key" ON "DynamicAccessKey"("path");
