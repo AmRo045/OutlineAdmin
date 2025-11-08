7766b1062b844abaacb2788c238754f3d610bcec
AmRo <VirtualEmailBox045@Gmail.Com>
2025-11-08 20:49:41 +0330

    Change the dak job to update the manual daks data usage too

--- scripts/dak-job.ts
+++ scripts/dak-job.ts
@@ -1,3 +1,5 @@
+import { DynamicAccessKey } from "@prisma/client";
+
 import prisma from "@/prisma/db";
 import { createLogger } from "@/src/core/logger";
 import { LoggerContext } from "@/src/core/definitions";
@@ -9,67 +11,101 @@ let logger = createLogger(LoggerContext.DakJob);
 const main = async () => {
     logger.info("Starting Dynamic Access Key maintenance job...");
 
-    const dynamicAccessKeys = await prisma.dynamicAccessKey.findMany({
-        where: { isSelfManaged: true }
-    });
+    const dynamicAccessKeys = await prisma.dynamicAccessKey.findMany();
 
-    logger.info(`Found ${dynamicAccessKeys.length} self-managed DAK(s) to process.`);
+    logger.info(`Found ${dynamicAccessKeys.length} DAK(s) to process.`);
+
+    const processSelfManagedDak = async (dak: DynamicAccessKey) => {
+        let keysRemovedDueToExpiration = false;
+
+        const expiryDate = getDakExpiryDateBasedOnValidityPeriod(dak);
+
+        if (expiryDate && expiryDate.getTime() <= Date.now()) {
+            logger.warn("DAK expired — removing access keys", {
+                id: dak.id,
+                name: dak.name,
+                expiryDate
+            });
+            await removeSelfManagedDynamicAccessKeyAccessKeys(dak.id);
+            keysRemovedDueToExpiration = true;
+        }
+
+        const pattern = `self-managed-dak-access-key-${dak.id}`;
+        const accessKeys = await prisma.accessKey.findMany({
+            where: { name: { contains: pattern } }
+        });
+
+        logger.info("Access keys found", {
+            id: dak.id,
+            name: dak.name,
+            count: accessKeys.length
+        });
+
+        const dataUsage = accessKeys.reduce((acc, key) => acc + Number(key.dataUsage || 0), 0);
+
+        await prisma.dynamicAccessKey.update({
+            where: { id: dak.id },
+            data: { dataUsage }
+        });
+
+        logger.info("Data usage updated", {
+            id: dak.id,
+            name: dak.name,
+            dataUsage
+        });
+
+        if (!keysRemovedDueToExpiration && dak.dataLimit) {
+            const bytesPerMB = 1024 * 1024;
+            const dataLimitInBytes = Number(dak.dataLimit) * bytesPerMB;
+
+            if (dataUsage >= dataLimitInBytes) {
+                logger.warn("DAK exceeded data limit — removing access keys", {
+                    id: dak.id,
+                    name: dak.name,
+                    dataUsage,
+                    dataLimitInBytes
+                });
+
+                await removeSelfManagedDynamicAccessKeyAccessKeys(dak.id);
+            }
+        }
+    };
+
+    const processManualDak = async (dak: DynamicAccessKey) => {
+        const accessKeys = await prisma.dynamicAccessKey
+            .findUnique({
+                where: { id: dak.id }
+            })
+            .accessKeys();
+
+        if (!accessKeys) {
+            logger.info("DAK has no access keys");
+
+            return;
+        }
+
+        const dataUsage = accessKeys.reduce((acc, key) => acc + Number(key.dataUsage || 0), 0);
+
+        await prisma.dynamicAccessKey.update({
+            where: { id: dak.id },
+            data: { dataUsage }
+        });
+
+        logger.info("Data usage updated", {
+            id: dak.id,
+            name: dak.name,
+            dataUsage
+        });
+    };
 
     for (const dak of dynamicAccessKeys) {
         try {
             logger.info("Checking DAK", { id: dak.id, name: dak.name });
 
-            let keysRemovedDueToExpiration = false;
-
-            const expiryDate = getDakExpiryDateBasedOnValidityPeriod(dak);
-
-            if (expiryDate && expiryDate.getTime() <= Date.now()) {
-                logger.warn("DAK expired — removing access keys", {
-                    id: dak.id,
-                    name: dak.name,
-                    expiryDate
-                });
-                await removeSelfManagedDynamicAccessKeyAccessKeys(dak.id);
-                keysRemovedDueToExpiration = true;
-            }
-
-            const pattern = `self-managed-dak-access-key-${dak.id}`;
-            const accessKeys = await prisma.accessKey.findMany({
-                where: { name: { contains: pattern } }
-            });
-
-            logger.info("Access keys found", {
-                id: dak.id,
-                name: dak.name,
-                count: accessKeys.length
-            });
-
-            const dataUsage = accessKeys.reduce((acc, key) => acc + Number(key.dataUsage || 0), 0);
-
-            await prisma.dynamicAccessKey.update({
-                where: { id: dak.id },
-                data: { dataUsage }
-            });
-
-            logger.info("Data usage updated", {
-                id: dak.id,
-                name: dak.name,
-                dataUsage
-            });
-
-            if (!keysRemovedDueToExpiration && dak.dataLimit) {
-                const bytesPerMB = 1024 * 1024;
-                const dataLimitInBytes = Number(dak.dataLimit) * bytesPerMB;
-
-                if (dataUsage >= dataLimitInBytes) {
-                    logger.warn("DAK exceeded data limit — removing access keys", {
-                        id: dak.id,
-                        name: dak.name,
-                        dataUsage,
-                        dataLimitInBytes
-                    });
-                    await removeSelfManagedDynamicAccessKeyAccessKeys(dak.id);
-                }
+            if (dak.isSelfManaged) {
+                await processSelfManagedDak(dak);
+            } else {
+                await processManualDak(dak);
             }
 
             logger.info("Finished processing DAK", { id: dak.id, name: dak.name });
